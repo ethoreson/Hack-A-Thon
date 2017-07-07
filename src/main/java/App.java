dcf81221a12ee4600e2094a961943616e75b3c4f
Emilie Thoreson <thoresonemilie@gmail.com>
2017-07-07 10:31:05 -0700

    fix index bug, now homepage is displayed

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,7 @@
 import java.util.Map;
 import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
@@ -10,8 +12,27 @@ public class App {
     String layout = "templates/layout.vtl";
 
     get("/", (request, response) -> {
+
       Map<String, Object> model = new HashMap<String, Object>();
-      model.put("template", "templates/index.vtl");
+      model.put("teams", request.session().attribute("teams"));
+      model.put("templates", "templates/index.vtl");
+      return new ModelAndView(new HashMap(), "templates/index.vtl");
+    }, new VelocityTemplateEngine());
+
+    post("/teams", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+
+      ArrayList<Team> teams = request.session().attribute("teams");
+      if (teams == null) {
+        teams = new ArrayList<Team>();
+        request.session().attribute("teams", teams);
+      }
+
+      String teamName = request.queryParams("teamName");
+      Team newTeam = new Team(teamName);
+      teams.add(newTeam);
+
+      model.put("template", "templates/success.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
